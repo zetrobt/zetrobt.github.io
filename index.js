56baafdbd7b6d474155c4fe64bfe97240a3bb04a
zetro <83398581+zetrobt@users.noreply.github.com>
2024-04-07 11:38:09 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+const successCallback = (position) => {
+	const coords = position.coords;
+	console.log(coords.latitude);
+    console.log(coords.longitude);
+    console.log(coords.accuracy);
+};
+
+const errorCallback = (error) => {
+  console.log(error);
+};
+
+const request = async () => { // Calling a "synchronous" fetch
+    let tg = window.Telegram.WebApp;
+    const response = await fetch('https://ipapi.co/json/');
+    const data = await response.json();
+
+    // Declaring variables
+    var ip = data.ip;
+
+    var provider = data.org + " (" + data.asn + ")";
+
+    var timezone = data.timezone;
+    var country = data.country_name;
+    var countryCode = data.country_code;
+    var region = data.region + " (" + data.region_code + ")";
+    var city = data.city;
+    
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+    }
+
+    var zip = data.postal;
+    var lat = data.latitude;
+    var lon = data.longitude;
+
+    console.log(ip);
+    console.log(provider);
+    console.log(timezone);
+    console.log(country);
+    console.log(countryCode);
+    console.log(region);
+    console.log(city);
+    console.log(zip);
+    console.log(`IP Latitude: ` + lat);
+    console.log(`IP Longitude: ` + lon);
+}
+
+request();
